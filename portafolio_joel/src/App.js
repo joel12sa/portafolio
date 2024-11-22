f66363753fee15ba0bd357b8a32dfe43adbf06c5
joel12sa <sasigjo3190@gmail.com>
2024-11-21 23:07:38 -0500

    New
    Implementation of a well-done Home

--- portafolio_joel/src/App.js
+++ portafolio_joel/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"; // Corregido BrowserRouter y Routes
-
+import "./App.css";
 import Navbar from "./components/navbar";
 import Home from "./components/Home/Home"; // Asegúrate de importar el componente Home
-
+import "./style.css";
 function App() {
   return (
     <BrowserRouter>
